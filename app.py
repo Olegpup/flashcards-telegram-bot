3d7261f114bf45b7d46d2bda75b52e295048d548
kiwvic <torogao@protonmail.com>
2022-07-05 22:29:10 +0300

    Add logging into console

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from aiogram import executor
 from loader import dp
 from handlers import *
+from logging import basicConfig, INFO
+
+
+basicConfig(level=INFO)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
