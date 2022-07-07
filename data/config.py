3e62c0e4bd4e53e41a760cc6f9f6dd28b00c2980
Oleg <ddsddfds@gmail.com>
2022-07-07 15:15:47 +0300

    fix .env

--- data/config.py
+++ data/config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(".env")
+load_dotenv("../.env")
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 CONNECT_STR_MONGO = str(os.getenv("CONNECT_STR_MONGO"))
