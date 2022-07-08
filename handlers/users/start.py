405067dbec090a4f2c008900d4ef37dcebc4e5c2
Oleg <ddsddfds@gmail.com>
2022-07-08 19:10:22 +0300

    add first handler

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,11 +1,15 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-from keyboards.inline import kb_main_menu
+from keyboards.inline import *
 from loader import dp
 
 
 @dp.message_handler(commands=["start"])
 async def start_bot(message: types.Message):
-    await message.answer(f"Эхо без состояния."
-                         f"Сообщение:\n"
-                         f"{message.text}", reply_markup=kb_main_menu())
+    await message.answer("Main menu:", reply_markup=kb_main_menu())
+
+
+@dp.callback_query_handler(text="view_categories")
+async def view_categories(callback: types.CallbackQuery):
+    await callback.message.edit_text("Categories:", reply_markup=kb_categories_menu())
+
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -9,3 +9,11 @@ def kb_main_menu():
         InlineKeyboardButton(text="Settings", callback_data="view_settings"),
         *common_buttons
     ])
+
+
+def kb_categories_menu():
+    return InlineKeyboardMarkup(resize_keyboard=True).add(*[
+        InlineKeyboardButton(text="Add category", callback_data="add_category"),
+        InlineKeyboardButton(text="Delete category", callback_data="delete_category"),
+        *common_buttons
+    ])
