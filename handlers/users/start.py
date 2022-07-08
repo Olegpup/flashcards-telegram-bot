2c05489dba61e11e39c9e62b64f7492d186c4fdc
Oleg <ddsddfds@gmail.com>
2022-07-08 23:06:36 +0300

    add back button to categories

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,6 +9,11 @@ async def start_bot(message: types.Message):
     await message.answer("Main menu:", reply_markup=kb_main_menu())
 
 
+@dp.callback_query_handler(text="view_main_menu")
+async def view_categories(callback: types.CallbackQuery):
+    await callback.message.edit_text("Main menu:", reply_markup=kb_main_menu())
+
+
 @dp.callback_query_handler(text="view_categories")
 async def view_categories(callback: types.CallbackQuery):
     await callback.message.edit_text("Categories:", reply_markup=kb_categories_menu())
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -15,5 +15,6 @@ def kb_categories_menu():
     return InlineKeyboardMarkup(resize_keyboard=True).add(*[
         InlineKeyboardButton(text="Add category", callback_data="add_category"),
         InlineKeyboardButton(text="Delete category", callback_data="delete_category"),
+        InlineKeyboardButton(text="Back", callback_data="view_main_menu"),
         *common_buttons
     ])
