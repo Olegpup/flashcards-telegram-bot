b63b39e322316a432de6dfab7f69e465d65e6da7
kiwvic <torogao@protonmail.com>
2022-07-18 16:08:51 +0300

    move is_admin check to utils

--- handlers/private/main_menu.py
+++ handlers/private/main_menu.py
@@ -3,12 +3,12 @@ from consts.c_handlers import *
 from .fsm import CategoriesMenu
 from loader import dp
 from aiogram import types
-from config import ADMINS
+from .utils import is_admin
 
 
 @dp.message_handler(commands=["start"], state="*")
 async def start_bot(message: types.Message):
-    if message.from_user.id in ADMINS:
+    if is_admin(message.from_user.id):
         await message.answer(WELCOME_MESSAGE, reply_markup=kb_main_menu_admins())
     else:
         await message.answer(WELCOME_MESSAGE, reply_markup=kb_main_menu())
---
+++ handlers/private/utils.py
@@ -0,0 +1,4 @@
+from config import ADMINS
+
+def is_admin(id_):
+    return id_ in ADMINS
