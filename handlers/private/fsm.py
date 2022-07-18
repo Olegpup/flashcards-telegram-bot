b5d8359c707760c4bf9ab782e2f42b8024e0f220
kiwvic <torogao@protonmail.com>
2022-07-19 02:17:23 +0300

    add dynamic fsm init

--- handlers/private/fsm.py
+++ handlers/private/fsm.py
@@ -1,12 +1,31 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-class SettingsMenu(StatesGroup):
-    pass
+def _init_fsm_tree(name: str, states_tree_: dict) -> type:
+    states_tree = states_tree_
+    attributes = dict()
+
+    __states_tree_to_attributes(attributes, states_tree)
+
+    attributes.update({"states_tree", states_tree})
+
+    return type(name, (StatesGroup, ), attributes)
 
 
-class CategoriesMenu(StatesGroup):
-    categories = State()
-    decks = State()
-    deck = State()
-    cards = State()
+def __states_tree_to_attributes(attributes, level: dict):
+    for key, val in level.items():
+        attributes[key] = State()
+
+        if val:
+            __states_tree_to_attributes(attributes, val)
+
+
+CategoriesMenu = _init_fsm_tree(
+    "CategoriesMenu",
+    {
+        "categories": {},
+        "decks": {},
+        "deck": {},
+        "cards": {}
+    }
+)
