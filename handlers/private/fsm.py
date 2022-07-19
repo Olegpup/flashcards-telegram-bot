984467c01ddd17822c0a0fa574a71bd1050e4cfc
kiwvic <torogao@protonmail.com>
2022-07-19 19:07:30 +0300

    use list instead of dict in FSMNode

--- handlers/private/fsm.py
+++ handlers/private/fsm.py
@@ -6,18 +6,18 @@ class FSMNode:
     def __init__(self, value, parent, nodes):
         self.value: str = value
         self.parent: FSMNode = parent
-        self.nodes: dict = nodes
+        self.nodes: list = nodes
 
 
 def _init_fsm_tree(name: str, states_tree_: dict) -> type:
-    states_tree = states_tree_
+    states_tree = states_tree_  # TODO Remove/replace?
     attributes = dict()
 
     _states_tree_to_attributes(attributes, states_tree, None)
 
     first = list(states_tree.keys())[0]
     root = FSMNode(first, None, states_tree[first].nodes)
-    print(root.nodes["a"].nodes)
+
     attributes.update({"states_tree_root": root})
 
     return type(name, (StatesGroup, ), attributes)
@@ -30,7 +30,7 @@ def _states_tree_to_attributes(attributes, level: dict, parent: FSMNode or None)
         if val:
             _states_tree_to_attributes(attributes, val, level[key])
 
-        level[key] = FSMNode(key, parent, val)  # TODO use list instead dict
+        level[key] = FSMNode(key, parent, list(val.values())) 
 
 
 CategoriesMenu = _init_fsm_tree(
