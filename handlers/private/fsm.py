ba8fc00e2fc8df187c4ed4b784952435b537d729
kiwvic <torogao@protonmail.com>
2022-07-19 03:15:45 +0300

    add FSMNode root instead fsm_tree attribute

--- handlers/private/fsm.py
+++ handlers/private/fsm.py
@@ -1,29 +1,42 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from numpy import isin
+
+
+class FSMNode:
+    def __init__(self, value, parent, nodes):
+        self.value: str = value
+        self.parent: FSMNode = parent
+        self.nodes: dict = nodes
 
 
 def _init_fsm_tree(name: str, states_tree_: dict) -> type:
     states_tree = states_tree_
     attributes = dict()
 
-    __states_tree_to_attributes(attributes, states_tree)
+    _states_tree_to_attributes(attributes, states_tree, None)
 
-    attributes.update({"states_tree": states_tree})
+    first = list(states_tree.keys())[0]
+    root = FSMNode(first, None, states_tree[first].nodes)
+    print(root.nodes["a"].nodes)
+    attributes.update({"states_tree_root": root})
 
     return type(name, (StatesGroup, ), attributes)
 
 
-def __states_tree_to_attributes(attributes, level: dict):
+def _states_tree_to_attributes(attributes, level: dict, parent: FSMNode or None):
     for key, val in level.items():
         attributes[key] = State()
 
         if val:
-            __states_tree_to_attributes(attributes, val)
+            _states_tree_to_attributes(attributes, val, level[key])
+
+        level[key] = FSMNode(key, parent, val)  # TODO use list instead dict
 
 
 CategoriesMenu = _init_fsm_tree(
     "CategoriesMenu",
     {
-        "categories": {},
+        "categories": {"a": {"b": {"c": {}}, "f": {}}},
         "decks": {},
         "deck": {},
         "cards": {}
