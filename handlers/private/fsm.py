2e3cd200ad7a8e9d0c55859038db1e91fd4017ef
kiwvic <torogao@protonmail.com>
2022-07-19 20:04:09 +0300

    remade connecting nodes logic

--- handlers/private/fsm.py
+++ handlers/private/fsm.py
@@ -1,5 +1,4 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from numpy import isin
 
 
 class FSMNode:
@@ -9,36 +8,40 @@ class FSMNode:
         self.nodes: list = nodes
 
 
-def _init_fsm_tree(name: str, states_tree_: dict) -> type:
-    states_tree = states_tree_  # TODO Remove/replace?
+def _init_fsm_tree(name: str, states_tree: dict) -> type:
     attributes = dict()
 
-    _states_tree_to_attributes(attributes, states_tree, None)
-
-    first = list(states_tree.keys())[0]
-    root = FSMNode(first, None, states_tree[first].nodes)
+    root = _connect_nodes(attributes, states_tree, None)
 
     attributes.update({"states_tree_root": root})
 
     return type(name, (StatesGroup, ), attributes)
 
 
-def _states_tree_to_attributes(attributes, level: dict, parent: FSMNode or None):
-    for key, val in level.items():
-        attributes[key] = State()
+def _connect_nodes(attributes: dict, level: dict, parent: FSMNode or None) -> FSMNode:
+    for name, childs in level.items():
+        attributes[name] = State()
 
-        if val:
-            _states_tree_to_attributes(attributes, val, level[key])
+        current_node = FSMNode(name, parent, [])
 
-        level[key] = FSMNode(key, parent, list(val.values())) 
+        if parent:
+            parent.nodes.append(current_node)
+
+        if childs:
+            _connect_nodes(attributes, childs, current_node)
+
+        if current_node.parent is None:
+            return current_node
 
 
 CategoriesMenu = _init_fsm_tree(
     "CategoriesMenu",
     {
-        "categories": {"a": {"b": {"c": {}}, "f": {}}},
-        "decks": {},
-        "deck": {},
-        "cards": {}
+        "root": {
+            "categories": {"a": {"b": {"c": {}}, "f": {}}},
+            "decks": {},
+            "deck": {},
+            "cards": {}
+        }
     }
 )
