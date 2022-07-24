c873a663cf48e531720f62597f6d161dacde939e
kiwvic <torogao@protonmail.com>
2022-07-24 23:53:10 +0300

    add admins in .env

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv(os.getcwd() + "/.env")
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 CONNECT_STR_MONGO = str(os.getenv("CONNECT_STR_MONGO"))
+ADMINS = list(map(int, os.getenv("ADMINS").split(",")))
